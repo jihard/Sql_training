a752cb99cdc9f09f5598ae5023bf1f7c026a3db2
Jihard@inbox.lv <Jihard@inbox.lv>
2021-08-31 16:27:37 +0300

    Update

--- sql-movies/src/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Source data may contain ' and " which my broke your sql, escape them using this function.
- */
-export const escape = (source: string): string => {
-  return source.replace(/"/g, `'`).replace(/'/g, `''`);
-};
